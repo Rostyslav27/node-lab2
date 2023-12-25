90767b03549a8f92ec2573c348fc7b5562983e70
Rostyslav <roctik4x@gmail.com>
2023-12-25 21:00:50 +0200

    start server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const i: number = 1;
-console.log(i + 1);
+import http from "http";
+
+const server = http.createServer(async (req, res) => {
+  res.end("Hello world");
+});
+
+server.on("clientError", (err, socket) => {
+  socket.end("HTTP/1.1 400 Bd Request\r\n\r\n");
+});
+
+server.listen(+(process.env.PORT || 8000));
+
+process.on("SIGINT", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+});
